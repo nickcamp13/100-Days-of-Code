eb3482a7ba441794eae5b15e4adf4c3b8c5274de
Nick Campos <nickreyescampos@gmail.com>
2022-08-20 16:53:43 -1000

    started using Turtle and Screen

---
+++ Object-Oriented-Programming/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import prettytable
+nicky = Turtle()
+my_screen = Screen()
+
+nicky.shape("turtle")
+nicky.color("ForestGreen")
+nicky.forward(100)
+my_screen.exitonclick()
+
+print(nicky)
+print(my_screen.canvheight)
+
